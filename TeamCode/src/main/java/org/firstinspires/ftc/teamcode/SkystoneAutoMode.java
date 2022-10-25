44a350e0cae0add83383eae8415ff9f54c4fb1e2
witmana <67086636+Curiosity11770@users.noreply.github.com>
2022-10-25 15:00:21 -0700

    Update SkystoneAutoMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneAutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneAutoMode.java
@@ -1,5 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
-public class SkystoneAutoMode {
-    
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous
+public class SkystoneAutoMode extends LinearOpMode {
+    OpenCvCamera phoneCam;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+
+        SkystoneDetector detector = new SkystoneDetector(telemetry);
+
+        phoneCam.setPipeline(detector);
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                phoneCam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+
+    }
 }
